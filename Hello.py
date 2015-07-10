92318e73bfd59bb71407666500918c6a5c8202c2
樋口裕次郎 <higuch@higuchiyuujirou-no-MacBook-Pro.local>
2015-07-10 19:25:41 +0900

    ルーティングの追加

--- Hello.py
+++ Hello.py
@@ -1,11 +1,13 @@
-# flaskをimportしている
 from flask import Flask
 app = Flask(__name__)
 
-# このルートがないと動かないらしい
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
 
 if __name__ == '__main__':
-    app.run() # ルートを動かしているっぽい
+   app.run(debug=True)
