5e5d92b7d684f8e736baad5f5eb5fcea52b7943b
樋口裕次郎 <higuch@higuchiyuujirou-no-MacBook-Pro.local>
2015-07-10 19:07:30 +0900

    コメント追加

--- Hello.py
+++ Hello.py
@@ -1,9 +1,11 @@
+# flaskをimportしている
 from flask import Flask
 app = Flask(__name__)
 
+# このルートがないと動かないらしい
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run() # ルートを動かしているっぽい
